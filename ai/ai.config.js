7848a8bf64408915d46895e69525035664f3eb4b
Ali Raza <161970819+ALiRazax1@users.noreply.github.com>
2026-07-05 21:57:52 +0500

    Add dotenv for env variables

--- ai/ai.config.js
+++ ai/ai.config.js
@@ -1,5 +1,6 @@
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
-
+import dotenv from "dotenv";
+dotenv.config();
 export function getAgentModel() {
   const provider = createOpenRouter({ apiKey: process.env.OPENROUTER_API_KEY});
   const modelID = process.env.OPENROUTER_DEFAULT_MODEL;
